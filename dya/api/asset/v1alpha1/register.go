d96d7a9d2595ca19af86e93e587491eb6176bbf7
kryntelxf <woseltehateha@gmail.com>
2026-09-03 00:13:56 +0700

    Fix missing newline at end of register.go
    Add missing newline at the end of register.go file.

--- dya/api/asset/v1alpha1/register.go
+++ dya/api/asset/v1alpha1/register.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
