24889f337608f147ed466ecc335a449fd9b4255a
kryntelxf <woseltehateha@gmail.com>
2026-09-06 11:23:30 +0700

    Add BasicExecutor implementation for recovery steps

---
+++ dya/pkg/recovery/executors/basic_executor.go
@@ -0,0 +1,41 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package executors
+
+import (
+	"time"
+
+	"k8s.io/klog/v2"
+	"k8s.io/kubernetes/dya/pkg/recovery"
+)
+
+type BasicExecutor struct{}
+
+func (e *BasicExecutor) Name() string {
+	return "basic-executor"
+}
+
+func (e *BasicExecutor) Execute(step *recovery.Step) (*recovery.ExecutionResult, error) {
+	klog.V(4).Infof("Executing step: %s", step.Name)
+	return &recovery.ExecutionResult{
+		StepID:    step.ID,
+		Success:   true,
+		Message:   "Step executed successfully",
+		Duration:  "1s",
+		Timestamp: time.Now(),
+	}, nil
+}
