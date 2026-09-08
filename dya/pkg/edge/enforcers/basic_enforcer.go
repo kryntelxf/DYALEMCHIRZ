92f1070b3b55178b015e1ea1edb0c416826ef623
kryntelxf <woseltehateha@gmail.com>
2026-09-09 03:48:17 +0700

    Add BasicEnforcer implementation for policy enforcement

---
+++ dya/pkg/edge/enforcers/basic_enforcer.go
@@ -0,0 +1,32 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package enforcers
+
+import (
+	"k8s.io/klog/v2"
+)
+
+type BasicEnforcer struct{}
+
+func (e *BasicEnforcer) Name() string {
+	return "basic-enforcer"
+}
+
+func (e *BasicEnforcer) Enforce(policy interface{}) error {
+	klog.V(4).Info("BasicEnforcer enforcing policy")
+	return nil
+}
