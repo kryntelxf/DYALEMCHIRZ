4fdc809b189fe8abb3484f88b4823e742ce45507
kryntelxf <woseltehateha@gmail.com>
2026-09-09 09:33:32 +0700

    Implement BasicPredictor for predictions

---
+++ dya/pkg/predictive/predictors/basic_predictor.go
@@ -0,0 +1,42 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package predictors
+
+import (
+	"time"
+
+	"k8s.io/klog/v2"
+	"k8s.io/kubernetes/dya/pkg/predictive"
+)
+
+type BasicPredictor struct{}
+
+func (p *BasicPredictor) Name() string {
+	return "basic-predictor"
+}
+
+func (p *BasicPredictor) Predict(data interface{}) (*predictive.Prediction, error) {
+	klog.V(4).Info("BasicPredictor making prediction")
+	return &predictive.Prediction{
+		AssetID:     "unknown",
+		EventType:   "failure",
+		Probability: 0.05,
+		TimeFrame:   "24h",
+		Confidence:  85.0,
+		Timestamp:   time.Now(),
+	}, nil
+}
