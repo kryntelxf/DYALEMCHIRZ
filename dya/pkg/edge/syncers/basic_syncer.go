f519ac096c643d675709a7d8948742624e64868b
kryntelxf <woseltehateha@gmail.com>
2026-09-09 07:05:52 +0700

    Add BasicSyncer implementation for edge syncers

---
+++ dya/pkg/edge/syncers/basic_syncer.go
@@ -0,0 +1,40 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package syncers
+
+import (
+	"k8s.io/klog/v2"
+)
+
+// BasicSyncer is a basic edge syncer
+type BasicSyncer struct{}
+
+// NewBasicSyncer creates a new basic syncer
+func NewBasicSyncer() *BasicSyncer {
+	return &BasicSyncer{}
+}
+
+// Name returns the name of the syncer
+func (s *BasicSyncer) Name() string {
+	return "basic-syncer"
+}
+
+// Sync performs synchronization
+func (s *BasicSyncer) Sync() error {
+	klog.V(4).Info("BasicSyncer syncing")
+	return nil
+}
